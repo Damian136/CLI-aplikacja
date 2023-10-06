4669b30c1f29194f8f0ba69e69401be5f8d97595
Damian Nowak <108193148+Damian136@users.noreply.github.com>
2023-10-06 13:39:38 +0200

    update-contacts

--- contacts.js
+++ contacts.js
@@ -4,95 +4,58 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 function listContacts() {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return [];
-    }
-
-    try {
-      const contacts = JSON.parse(data);
-      console.table(contacts);
-      return contacts;
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  });
+  try {
+    const data = fs.readFileSync(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+    return contacts;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 function getContactById(contactId) {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return null;
-    }
-
-    try {
-      const contacts = JSON.parse(data);
-      const contact = contacts.find((c) => c.id === contactId);
-      console.log(contact);
-      return contact || null;
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  });
+  try {
+    const data = fs.readFileSync(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((c) => c.id === contactId);
+    console.log(contact);
+    return contact || null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 function removeContact(contactId) {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    try {
-      const contacts = JSON.parse(data);
-      const updatedContacts = contacts.filter((c) => c.id !== contactId);
-      fs.writeFile(
-        contactsPath,
-        JSON.stringify(updatedContacts, null, 2),
-        (err) => {
-          if (err) {
-            console.error(err);
-          }
-          console.log("Contact removed successfully.");
-        }
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  try {
+    const data = fs.readFileSync(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.filter((c) => c.id !== contactId);
+    fs.writeFileSync(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    console.log("Contact removed successfully.");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function addContact(name, email, phone) {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    try {
-      const contacts = JSON.parse(data);
-      const newContact = {
-        id: Date.now(),
-        name,
-        email,
-        phone,
-      };
-      contacts.push(newContact);
-
-      fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
-        if (err) {
-          console.error(err);
-        }
-        console.log("Contact added successfully.");
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  try {
+    const data = fs.readFileSync(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: Date.now(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log("Contact added successfully.");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
